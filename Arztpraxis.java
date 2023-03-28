48874d34784854b30a957fbf6ab2a41eb0736dfa
Wenisch Herbert <herbert.wenisch@t-online.de>
2023-03-28 15:10:43 +0200

    "b: kleiner 18: Kinderarzt"

--- Arztpraxis.java
+++ Arztpraxis.java
@@ -15,4 +15,10 @@ public class Arztpraxis{
         arzt1 = new ArrayList<Patient>();
         arzt2 = new ArrayList<Patient>();
     }
+    
+    public void anmelden(Patient p){
+        if(p.nenneAlter() < 18) kinderarzt.add(p);
+        else if (arzt1.size() < arzt2.size()) arzt1.add(p);
+             else arzt2.add(p);
+    }
 }
